7256d723f76ca057763d4e2c579f9e0b94e62984
kevin Pechersky <kev.pechersky@gmail.com>
2022-11-11 19:39:27 -0500

    Update wordcount.py

---
+++ wordcount.py
@@ -0,0 +1,21 @@
+
+
+file="test.txt"
+
+def read_file():
+    f = open(file, "r")
+
+def Open(fileName):
+	if (file name ends with “.txt”):
+return open(fileName, "r")
+else:
+	print(“not a txt file”)
+    exit(1)
+    def Read():
+    f = open(file, "r")
+    return f.read()
+
+    def Write():
+    f = open(file, "w")
+    return f.write(Read())
+    
