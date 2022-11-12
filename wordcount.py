0d90dff94def08d4ede199cfd488d94f87f5adfc
kevin Pechersky <kev.pechersky@gmail.com>
2022-11-11 19:41:33 -0500

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -17,3 +17,13 @@ def Write():
 g = open(file, "w")
 return g.write(Read())
 
+ def createListOfWords(fileObject):
+	# empty list
+wordList = [ ]
+#loop through all the lines in the file
+for line in fileObject:
+	# in each line split at space which will give the word
+	for word in line.split():
+		#add the word to the word list
+		wordList.append(word)
+return wordList
