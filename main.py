8001292eb4ea551de66ce4d1556b69d53109ad03
Jobanpreet Singh <63566188+programmerjoban@users.noreply.github.com>
2022-12-22 13:09:22 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-def createsSetOfUniqueWords(wordList):
-    return set(wordList)
-
-
-def countFrequency(wordList, setOfWords):
-    for words in setOfWords:
-        print(words+":"+str(wordList.count(words)))
