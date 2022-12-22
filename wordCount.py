8efb12327aabd2342de8c065f73945b9db877c25
kevin Pechersky <kev.pechersky@gmail.com>
2022-12-22 13:23:19 -0500

    added small Files function and time

--- wordCount.py
+++ wordCount.py
@@ -1,5 +1,33 @@
 import argparse
+import time
 import string
+import os
+from itertools import islice
+
+# break the large files into small files of 30000 lines
+size = 30000
+
+# This function breaks large txt file into small txt file
+# The input is a file name
+def createSmallFiles(inputFile):
+    # counter for the number subfile created
+    counter = 0
+    with open(inputFile,"r",encoding="utf8") as txtfile:
+        while True:
+            # list of 30000 lines
+            nextLines = list(islice(txtfile, size))
+
+            # small file number increase
+            counter += 1
+
+            # if there are no nextlines break the loop
+            if not nextLines:
+                break
+
+            # if nextlines exist write the small file
+            # small file name format = filename_smallFileNumber.txt
+            with open(inputFile.replace(".txt","_{}.txt".format(counter)), 'w', encoding="utf8") as outputFile:
+                outputFile.writelines(nextLines)
 
 def main():
     
@@ -26,6 +54,20 @@ def main():
     if(not(inputFile.endswith(".txt"))): 
         print("not a textFile")
         exit()
+        
+    # start timestamp to calculate execution time
+    t1 = time.time()
+
+    # divide the large text file into small files
+    createSmallFiles(inputFile)
+
+    #store the path of small files
+    smallFiles = []
+
+    #add the path to each small files to array smallFiles
+    for files in os.listdir('.'):
+        if files.startswith(inputFileName+"_"):
+            smallFiles.append(os.path.join('.',files))
 
 #when the script runs it will excecute the main function
 if __name__ == "__main__":
